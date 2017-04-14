e8877186ce44eb2945a2f66cb2cc57ede38e01d9
sok <theonesok@gmail.com>
2017-04-14 18:06:36 -0500

    primer commit

--- app/src/main/java/mx/buap/cs/sok/pruebacamara6/MainActivity.java
+++ app/src/main/java/mx/buap/cs/sok/pruebacamara6/MainActivity.java
@@ -1,13 +1,58 @@
 package mx.buap.cs.sok.pruebacamara6;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView camaraContenedor;
+    TextView   nombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button camara = (Button) findViewById(R.id.camara);
+        camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
+        camaraContenedor = (ImageView)findViewById(R.id.fotoContenedor);
+        nombre = (TextView)findViewById(R.id.nombre);
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            nombre.setText(takePictureIntent.getDataString());
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Bitmap escala=  Bitmap.createScaledBitmap(imageBitmap,512,512,true);
+            camaraContenedor.setImageBitmap(escala);
+        }
+    }
+
+
+
 }
